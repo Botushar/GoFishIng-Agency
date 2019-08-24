e957b64dc8df15542c633070e1603468f5086a79
Botushar <plamen305@gmail.com>
2019-08-24 23:08:57 +0300

    Some constants added.

--- GoFishIng.Common/GlobalConstants.cs
+++ GoFishIng.Common/GlobalConstants.cs
@@ -5,5 +5,13 @@
         public const string AdministratorRoleName = "Administrator";
 
         public const string UserRoleName = "User";
+
+        public const string Madeira = "MadeiraFishingChallenges";
+
+        public const string Bosnia = "BosniaFishingChallenges";
+
+        public const string Greece = "GreeceFishingChallenges";
+
+        public const decimal PriceMadeira = 2500M;
     }
 }
